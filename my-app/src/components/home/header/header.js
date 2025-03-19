0e503cd3ef515cda408d17857484e5137b5ab7ac
Ali <mali101908@gmail.com>
2025-03-19 22:13:57 +0500

    worked on responsivenss

--- my-app/src/components/home/header/header.js
+++ my-app/src/components/home/header/header.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import huluImage from '../../../images/hulu-logo.jpg';  // Correct import
-import '../home.css'
-import HeroSection from '../hero-section/hero'
+import React, { useState } from 'react';
+import '../home.css';
+import HeroSection from '../hero-section/hero';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle menu visibility
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-	<div className='container'>
-    <div className='navbar'>
-      {/* <img className='logo' src={huluImage} alt="Hulu Logo" width={100} height={80} /> */}
-      <h2 className='logo'>hulu</h2>
-	  <button className='log-btn'>LOG IN</button>
+    <div className='container'>
+      <div className='navbar'>
+        {/* Logo */}
+        <h2 className='logo'>hulu</h2>
+
+        {/* Desktop login button */}
+        <button className='log-btn'>LOG IN</button>
+        <button className='sign-up-btn'>SIGN UP</button>
+
+        {/* Hamburger menu for mobile screens */}
+        <div className='menu-icon' onClick={toggleMenu}>
+          &#9776;
+        </div>
+
+        {/* Mobile version buttons (Login and Sign Up) */}
+        <div className={`mobile-menu ${isMenuOpen ? 'active' : ''}`}>
+          <button className='log-btn-mobile'>LOG IN</button>
+          <button className='sign-up-btn-mobile'>SIGN UP</button>
+        </div>
+      </div>
+
+      <HeroSection />
     </div>
-	<HeroSection />
-	</div>
   );
 };
 
